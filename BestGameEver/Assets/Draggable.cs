89c4e1f1ef3739c53ef1fc2e3aad553fe91451ba
ErLoriz <46562693+ErLoriz@users.noreply.github.com>
2020-03-31 14:01:48 +0200

    Las cartas se cambian de sitio ahora
    Mola

--- BestGameEver/Assets/Draggable.cs
+++ BestGameEver/Assets/Draggable.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
 
 public class Draggable : MonoBehaviour , IBeginDragHandler , IDragHandler , IEndDragHandler
 {
 
     public Transform parentToReturnTo = null;
+
+    GameObject placeholder = null;
 
     public enum Slot { MANO, CAMPO };
     public Slot tipoCarta = Slot.MANO;
@@ -14,6 +18,16 @@ public class Draggable : MonoBehaviour , IBeginDragHandler , IDragHandler , IEnd
     public void OnBeginDrag(PointerEventData eventData)
     {
         Debug.Log("OnBeginDrag");
+
+        placeholder = new GameObject();
+        placeholder.transform.SetParent(this.transform.parent);
+        LayoutElement le = placeholder.AddComponent<LayoutElement>();
+        le.preferredWidth = placeholder.AddComponent<LayoutElement>().preferredWidth;
+        le.preferredHeight = placeholder.AddComponent<LayoutElement>().preferredHeight;
+        le.flexibleWidth = 0;
+        le.flexibleHeight = 0;
+
+        placeholder.transform.SetSiblingIndex(this.transform.GetSiblingIndex());
 
         parentToReturnTo = this.transform.parent;
         this.transform.SetParent(this.transform.parent.parent);
@@ -28,6 +42,32 @@ public class Draggable : MonoBehaviour , IBeginDragHandler , IDragHandler , IEnd
         //Debug.Log("OnDrag");
 
         this.transform.position = eventData.position;
+
+        int newSiblingIndex = placeholder.transform.GetSiblingIndex();
+
+        for (int i = 0; i < parentToReturnTo.childCount; i++)
+        {
+
+            if(this.transform.position.x < parentToReturnTo.GetChild(i).position.x)
+            {
+
+                newSiblingIndex = i;
+
+                if (placeholder.transform.GetSiblingIndex() < newSiblingIndex)
+                {
+
+                    newSiblingIndex--;
+                    break;
+
+                }
+
+                placeholder.transform.SetSiblingIndex(newSiblingIndex);
+                break;
+
+            }
+
+        }
+
     }
 
     public void OnEndDrag(PointerEventData eventData)
@@ -35,11 +75,12 @@ public class Draggable : MonoBehaviour , IBeginDragHandler , IDragHandler , IEnd
         Debug.Log("OnEndDrag");
 
         this.transform.SetParent(parentToReturnTo);
+        this.transform.SetSiblingIndex(placeholder.transform.GetSiblingIndex());
         GetComponent<CanvasGroup>().blocksRaycasts = true;
         tipoCarta = Slot.CAMPO;
 
         //EventSystem.current.RaycastAll(eventData);
-
+        Destroy(placeholder);
     }
 
 }
