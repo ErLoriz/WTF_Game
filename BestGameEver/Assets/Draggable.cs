f50267d69769151d131f9b119011fe5d21229922
ErLoriz <46562693+ErLoriz@users.noreply.github.com>
2020-04-06 16:22:59 +0200

    Hay mano y campo enemigo
    Poca cosa mas

--- BestGameEver/Assets/Draggable.cs
+++ BestGameEver/Assets/Draggable.cs
@@ -12,69 +12,80 @@ public class Draggable : MonoBehaviour , IBeginDragHandler , IDragHandler , IEnd
     public Transform placeHolderParent = null;
     GameObject placeholder = null;
 
-    public enum Slot { MANO, CAMPO };
+    public enum Slot { MANO, CAMPO, MANO_ENEMIGO, CAMPO_ENEMIGO };
     public Slot tipoCarta = Slot.MANO;
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-        Debug.Log("OnBeginDrag");
+        
+        if (tipoCarta == Slot.MANO || tipoCarta == Slot.MANO_ENEMIGO)
+        {
+            Debug.Log("OnBeginDrag");
+            placeholder = new GameObject();
+            placeholder.transform.SetParent(this.transform.parent);
+            LayoutElement le = placeholder.AddComponent<LayoutElement>();
+            le.preferredWidth = placeholder.AddComponent<LayoutElement>().preferredWidth;
+            le.preferredHeight = placeholder.AddComponent<LayoutElement>().preferredHeight;
+            le.flexibleWidth = 0;
+            le.flexibleHeight = 0;
 
-        placeholder = new GameObject();
-        placeholder.transform.SetParent(this.transform.parent);
-        LayoutElement le = placeholder.AddComponent<LayoutElement>();
-        le.preferredWidth = placeholder.AddComponent<LayoutElement>().preferredWidth;
-        le.preferredHeight = placeholder.AddComponent<LayoutElement>().preferredHeight;
-        le.flexibleWidth = 0;
-        le.flexibleHeight = 0;
+            placeholder.transform.SetSiblingIndex(this.transform.GetSiblingIndex());
 
-        placeholder.transform.SetSiblingIndex(this.transform.GetSiblingIndex());
+            parentToReturnTo = this.transform.parent;
+            placeHolderParent = parentToReturnTo;
+            this.transform.SetParent(this.transform.parent.parent);
 
-        parentToReturnTo = this.transform.parent;
-        placeHolderParent = parentToReturnTo;
-        this.transform.SetParent(this.transform.parent.parent);
+            GetComponent<CanvasGroup>().blocksRaycasts = false;
 
-        GetComponent<CanvasGroup>().blocksRaycasts = false;
-
+        }
+        
         //DropZone[] zones = GameObject.FindObjectOfType<DropZone>();
     }
 
     public void OnDrag(PointerEventData eventData)
     {
         //Debug.Log("OnDrag");
-
-        this.transform.position = eventData.position;
-
-        if (placeholder.transform.parent != placeHolderParent)
-            placeholder.transform.SetParent(placeHolderParent);
-
-        int newSiblingIndex = placeHolderParent.childCount;
-
-        for (int i = 0; i < placeHolderParent.childCount; i++)
+        if (tipoCarta == Slot.MANO || tipoCarta == Slot.MANO_ENEMIGO)
         {
-            if(this.transform.position.x < placeHolderParent.GetChild(i).position.x)
+            this.transform.position = eventData.position;
+
+            if (placeholder.transform.parent != placeHolderParent)
+                placeholder.transform.SetParent(placeHolderParent);
+
+            int newSiblingIndex = placeHolderParent.childCount;
+
+            for (int i = 0; i < placeHolderParent.childCount; i++)
             {
-                newSiblingIndex = i;
-                if (placeholder.transform.GetSiblingIndex() < newSiblingIndex)
+                if (this.transform.position.x < placeHolderParent.GetChild(i).position.x)
                 {
-                    newSiblingIndex--;
-                    break;
+                    newSiblingIndex = i;
+                    if (placeholder.transform.GetSiblingIndex() < newSiblingIndex)
+                    {
+                        newSiblingIndex--;
+                        break;
+                    }
                 }
+                placeholder.transform.SetSiblingIndex(newSiblingIndex);
             }
-            placeholder.transform.SetSiblingIndex(newSiblingIndex);
         }
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        Debug.Log("OnEndDrag");
 
-        this.transform.SetParent(parentToReturnTo);
-        this.transform.SetSiblingIndex(placeholder.transform.GetSiblingIndex());
-        GetComponent<CanvasGroup>().blocksRaycasts = true;
-        tipoCarta = Slot.CAMPO;
+        if (tipoCarta == Slot.MANO || tipoCarta == Slot.MANO_ENEMIGO)
+        {
 
-        //EventSystem.current.RaycastAll(eventData);
-        Destroy(placeholder);
+            Debug.Log("OnEndDrag");
+
+            this.transform.SetParent(parentToReturnTo);
+            this.transform.SetSiblingIndex(placeholder.transform.GetSiblingIndex());
+            GetComponent<CanvasGroup>().blocksRaycasts = true;
+            tipoCarta = Slot.CAMPO;
+
+            //EventSystem.current.RaycastAll(eventData);
+            Destroy(placeholder);
+        }
     }
 
 }
