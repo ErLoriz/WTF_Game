c7a52b27a97fb8bc69662cb471c9a9239b9e6075
Diego <diego.gomezur@elorrieta-errekamari.com>
2020-04-14 16:03:30 +0200

    TiendaCartas
    Tienda de cartas

---
+++ BestGameEver/Assets/OpenPanel_Cartas.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenPanel_Cartas : MonoBehaviour
+{
+
+    public GameObject Panel_Cartas;
+    public bool Open = false;
+    public void OpenPanel()
+    
+    {
+        if(Open == false)
+        {
+            Panel_Cartas.SetActive(true);
+            Open = true;
+        }
+        else if(Open == true)
+        {
+            Panel_Cartas.SetActive(false);
+            Open = false;
+        }
+   
+
+       
+     
+    }
+  
+}
