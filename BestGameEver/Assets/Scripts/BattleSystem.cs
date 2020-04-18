4ded205505b3c2e3fc1b0eb8162fc8c3c2df323d
ErLoriz <46562693+ErLoriz@users.noreply.github.com>
2020-04-18 16:00:13 +0200

    En nada empezamos con las peleas
    Cada vez queda menos
    
    Co-Authored-By: diegogomezuri <diegogomezuri@users.noreply.github.com>

---
+++ BestGameEver/Assets/Scripts/BattleSystem.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Threading;
+
+public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
+
+public class BattleSystem : MonoBehaviour
+{
+
+    // Thread tt = new Thread(ThreadTiempo.ThreadTemp);
+
+    public int CartasManoCant;
+    public GameObject Carta;
+    public GameObject[] CartasMano;
+    public Button botonPasar;
+
+    public Text TextoTurno;
+
+    public Canvas manoEnemiga;
+
+    public BattleState state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = BattleState.START;
+        SetupBattle();
+    }
+
+    void SetupBattle()
+    {
+        //Saldra una pantalla con 1 carta a elegir
+
+        var number = Random.Range(1, 3);
+
+        TextoTurno.text = "Turno " + number.ToString();
+
+        if (number == 1)
+        {
+
+            state = BattleState.PLAYERTURN;
+            PlayerTurn();
+        }
+        else
+        {
+
+            state = BattleState.ENEMYTURN;
+            EnemyTurn();
+        }
+        //Debug.Log("Hay " + manoEnemiga.FindGameObjectsWithTag("Carta0").Length);
+
+
+    }
+
+    public void PlayerTurn()
+    {
+        botonPasar.enabled = true;
+        TextoTurno.text = "Tu turno";
+
+    }
+
+    public void OnClickButtonTurn()
+    {
+
+        state = BattleState.ENEMYTURN;
+        EnemyTurn();
+
+    }
+
+
+    async void EnemyTurn()
+    {
+        botonPasar.enabled = false;
+        //   tt.Start();
+        TextoTurno.text = "Enemigo";
+        Debug.Log("Inicia el turno del enemigo");
+        await TimeForAIAsync();
+        TurnBuy();
+        await TimeForAIAsync();
+        TurnCards();
+        await TimeForAIAsync();
+        TurnBatle();
+        Debug.Log("Fin del turno del enemigo");
+
+        TextoTurno.text = "Enemigo";
+
+        state = BattleState.PLAYERTURN;
+        PlayerTurn();
+
+    }
+
+
+    void TurnBuy()//Compra
+    {
+
+    }
+
+    async void TurnCards()//Mueve cartas de la mano al campo
+    {
+        CartasManoCant = GameObject.FindGameObjectsWithTag("CartasManoEn").Length;
+        Debug.Log("El enemigo tiene: " + CartasManoCant + " cartas en la mano");
+
+
+        var ramd = Random.Range(0, GameObject.FindGameObjectsWithTag("CartasManoEn").Length);
+        var n = 0;
+        do // Elije el numero de carta que va a sacar de forma aleatoria
+        {
+            if (GameObject.FindGameObjectsWithTag("CartasManoEn").Length > 0) // Selecciona una carta aleatoria de la mano
+            {
+                CartasMano = GameObject.FindGameObjectsWithTag("CartasManoEn");
+                var selecciondeCarta = Random.Range(0, GameObject.FindGameObjectsWithTag("CartasManoEn").Length);
+                Carta = CartasMano[selecciondeCarta];
+                Carta.transform.gameObject.tag = "CartaCampoEn";
+                Instantiate(Carta, new Vector3(185, 165, 0), Quaternion.identity, GameObject.FindGameObjectWithTag("CampoEnemigo").transform);
+                DestroyImmediate(Carta);
+
+            }
+            n++;
+            await TimeForAIAsync();
+        } while (n < ramd);
+
+
+
+    }
+
+    void TurnBatle()//Lucha
+    {
+
+    }
+
+    void TimeForAI()
+    {
+        //  int time = Random.Range(500, 1500);
+        Thread.Sleep(1000);
+    }
+
+
+    async System.Threading.Tasks.Task<bool> TimeForAIAsync()
+    {
+        bool bol = true;
+        await System.Threading.Tasks.Task.Run(() =>
+        {
+            TimeForAI();
+        }
+        );
+        return bol;
+    }
+
+}
---
+++ BestGameEver/Assets/Scripts/TiendaCartas.cs
@@ -0,0 +1,195 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class TiendaCartas : MonoBehaviour
+{
+
+    private GameObject Carta0;
+    private GameObject Carta1;
+    private GameObject Carta2;
+    private GameObject Carta3;
+
+    static GameObject CartaPanel1 = null;
+    static GameObject CartaPanel2 = null;
+    static GameObject CartaPanel3 = null;
+    static GameObject CartaPanel4 = null;
+
+    public Transform Panel_Cartas;
+
+    void Start()
+    {
+
+        nuevasCartas();
+
+    }
+
+    void nuevasCartas()
+    {
+
+        Carta0 = Resources.Load("Carta0") as GameObject;
+        Carta1 = Resources.Load("Carta1") as GameObject;
+        Carta2 = Resources.Load("Carta2") as GameObject;
+        Carta3 = Resources.Load("Carta3") as GameObject;
+
+        var primera = Random.Range(1, 5);
+        var segunda = Random.Range(1, 5);
+        var tercero = Random.Range(1, 5);
+        var cuarto = Random.Range(1, 5);
+
+        if (CartaPanel1 != null)
+        {
+            DestroyImmediate(CartaPanel1);
+            DestroyImmediate(CartaPanel2);
+            DestroyImmediate(CartaPanel3);
+            DestroyImmediate(CartaPanel4);
+        
+            switch (primera)
+            {
+                case 1:
+                    CartaPanel1 = Instantiate(Carta0, new Vector3(185, 165, 0), Quaternion.identity, GameObject.FindGameObjectWithTag("PanelCartas1").transform);
+                    break;
+                case 2:
+                    CartaPanel1 = Instantiate(Carta1, new Vector3(185, 165, 0), Quaternion.identity, GameObject.FindGameObjectWithTag("PanelCartas1").transform);
+                    break;
+                case 3:
+                    CartaPanel1 = Instantiate(Carta2, new Vector3(185, 165, 0), Quaternion.identity, GameObject.FindGameObjectWithTag("PanelCartas1").transform);
+                    break;
+                case 4:
+                    CartaPanel1 = Instantiate(Carta3, new Vector3(185, 165, 0), Quaternion.identity, GameObject.FindGameObjectWithTag("PanelCartas1").transform);
+                    break;
+            }
+
+            switch (segunda)
+            {
+                case 1:
+                    CartaPanel2 = Instantiate(Carta0, new Vector3(185, 165, 0), Quaternion.identity, GameObject.FindGameObjectWithTag("PanelCartas2").transform);
+                    break;
+                case 2:
+                    CartaPanel2 = Instantiate(Carta1, new Vector3(185, 165, 0), Quaternion.identity, GameObject.FindGameObjectWithTag("PanelCartas2").transform);
+                    break;
+                case 3:
+                    CartaPanel2 = Instantiate(Carta2, new Vector3(185, 165, 0), Quaternion.identity, GameObject.FindGameObjectWithTag("PanelCartas2").transform);
+                    break;
+                case 4:
+                    CartaPanel2 = Instantiate(Carta3, new Vector3(185, 165, 0), Quaternion.identity, GameObject.FindGameObjectWithTag("PanelCartas2").transform);
+                    break;
+            }
+
+            switch (tercero)
+            {
+                case 1:
+                    CartaPanel3 = Instantiate(Carta0, new Vector3(185, 165, 0), Quaternion.identity, GameObject.FindGameObjectWithTag("PanelCartas3").transform);
+                    break;
+                case 2:
+                    CartaPanel3 = Instantiate(Carta1, new Vector3(185, 165, 0), Quaternion.identity, GameObject.FindGameObjectWithTag("PanelCartas3").transform);
+                    break;
+                case 3:
+                    CartaPanel3 = Instantiate(Carta2, new Vector3(185, 165, 0), Quaternion.identity, GameObject.FindGameObjectWithTag("PanelCartas3").transform);
+                    break;
+                case 4:
+                    CartaPanel3 = Instantiate(Carta3, new Vector3(185, 165, 0), Quaternion.identity, GameObject.FindGameObjectWithTag("PanelCartas3").transform);
+                    break;
+            }
+
+            switch (cuarto)
+            {
+                case 1:
+                    CartaPanel4 = Instantiate(Carta0, new Vector3(185, 165, 0), Quaternion.identity, GameObject.FindGameObjectWithTag("PanelCartas4").transform);
+                    break;
+                case 2:
+                    CartaPanel4 = Instantiate(Carta1, new Vector3(185, 165, 0), Quaternion.identity, GameObject.FindGameObjectWithTag("PanelCartas4").transform);
+                    break;
+                case 3:
+                    CartaPanel4 = Instantiate(Carta2, new Vector3(185, 165, 0), Quaternion.identity, GameObject.FindGameObjectWithTag("PanelCartas4").transform);
+                    break;
+                case 4:
+                    CartaPanel4 = Instantiate(Carta3, new Vector3(185, 165, 0), Quaternion.identity, GameObject.FindGameObjectWithTag("PanelCartas4").transform);
+                    break;
+            }
+        } else if(CartaPanel1 == null)
+        {
+            switch (primera)
+            {
+                case 1:
+                    CartaPanel1 = Instantiate(Carta0, new Vector3(185, 165, 0), Quaternion.identity, GameObject.FindGameObjectWithTag("PanelCartas1").transform);
+                    break;
+                case 2:
+                    CartaPanel1 = Instantiate(Carta1, new Vector3(185, 165, 0), Quaternion.identity, GameObject.FindGameObjectWithTag("PanelCartas1").transform);
+                    break;
+                case 3:
+                    CartaPanel1 = Instantiate(Carta2, new Vector3(185, 165, 0), Quaternion.identity, GameObject.FindGameObjectWithTag("PanelCartas1").transform);
+                    break;
+                case 4:
+                    CartaPanel1 = Instantiate(Carta3, new Vector3(185, 165, 0), Quaternion.identity, GameObject.FindGameObjectWithTag("PanelCartas1").transform);
+                    break;
+            }
+
+            switch (segunda)
+            {
+                case 1:
+                    CartaPanel2 = Instantiate(Carta0, new Vector3(185, 165, 0), Quaternion.identity, GameObject.FindGameObjectWithTag("PanelCartas2").transform);
+                    break;
+                case 2:
+                    CartaPanel2 = Instantiate(Carta1, new Vector3(185, 165, 0), Quaternion.identity, GameObject.FindGameObjectWithTag("PanelCartas2").transform);
+                    break;
+                case 3:
+                    CartaPanel2 = Instantiate(Carta2, new Vector3(185, 165, 0), Quaternion.identity, GameObject.FindGameObjectWithTag("PanelCartas2").transform);
+                    break;
+                case 4:
+                    CartaPanel2 = Instantiate(Carta3, new Vector3(185, 165, 0), Quaternion.identity, GameObject.FindGameObjectWithTag("PanelCartas2").transform);
+                    break;
+            }
+
+            switch (tercero)
+            {
+                case 1:
+                    CartaPanel3 = Instantiate(Carta0, new Vector3(185, 165, 0), Quaternion.identity, GameObject.FindGameObjectWithTag("PanelCartas3").transform);
+                    break;
+                case 2:
+                    CartaPanel3 = Instantiate(Carta1, new Vector3(185, 165, 0), Quaternion.identity, GameObject.FindGameObjectWithTag("PanelCartas3").transform);
+                    break;
+                case 3:
+                    CartaPanel3 = Instantiate(Carta2, new Vector3(185, 165, 0), Quaternion.identity, GameObject.FindGameObjectWithTag("PanelCartas3").transform);
+                    break;
+                case 4:
+                    CartaPanel3 = Instantiate(Carta3, new Vector3(185, 165, 0), Quaternion.identity, GameObject.FindGameObjectWithTag("PanelCartas3").transform);
+                    break;
+            }
+
+            switch (cuarto)
+            {
+                case 1:
+                    CartaPanel4 = Instantiate(Carta0, new Vector3(185, 165, 0), Quaternion.identity, GameObject.FindGameObjectWithTag("PanelCartas4").transform);
+                    break;
+                case 2:
+                    CartaPanel4 = Instantiate(Carta1, new Vector3(185, 165, 0), Quaternion.identity, GameObject.FindGameObjectWithTag("PanelCartas4").transform);
+                    break;
+                case 3:
+                    CartaPanel4 = Instantiate(Carta2, new Vector3(185, 165, 0), Quaternion.identity, GameObject.FindGameObjectWithTag("PanelCartas4").transform);
+                    break;
+                case 4:
+                    CartaPanel4 = Instantiate(Carta3, new Vector3(185, 165, 0), Quaternion.identity, GameObject.FindGameObjectWithTag("PanelCartas4").transform);
+                    break;
+            }
+        }
+    }
+
+    void cartasAMano(PointerEventData eventData)
+    {
+
+
+
+    }
+     
+    void reroll()
+    { 
+        nuevasCartas();
+    }
+    
+}
+
+
+
